2af8d0147ee26ffeb7178653451cd84a1a15f668
Ricardo Boss <mail@ricardoboss.de>
2024-09-26 16:50:53 +0200

    Added CSS variables and ability to use different colors and opacities for the layers

--- DuoBlazorIcons.Generator/IconTemplate.Code.cs
+++ DuoBlazorIcons.Generator/IconTemplate.Code.cs
@@ -2,6 +2,18 @@
 
 public partial class IconTemplate(IconMetadata metadata)
 {
+	private static string GetFillExpression(SvgPath path)
+	{
+		var cssPropertyName = path.Layer switch
+		{
+			PathLayer.Primary => "--duo-blazor-icon-primary-color",
+			PathLayer.Secondary => "--duo-blazor-icon-secondary-color",
+			_ => "--duo-blazor-icon-default-color",
+		};
+
+		return $"var({cssPropertyName}, currentColor)";
+	}
+
 	private static string GetOpacityExpression(SvgPath path)
 	{
 		var opacityOverridePropertyName = path.Layer switch
@@ -12,7 +24,15 @@ public partial class IconTemplate(IconMetadata metadata)
 		};
 
 		var fallbackOpacity = path.Opacity ?? 1m;
+		var cssPropertyName = path.Layer switch
+		{
+			PathLayer.Primary => "--duo-blazor-icon-primary-opacity",
+			PathLayer.Secondary => "--duo-blazor-icon-secondary-opacity",
+			_ => "--duo-blazor-icon-default-opacity",
+		};
 
-		return $"@({opacityOverridePropertyName} ?? {fallbackOpacity}m)";
+		var cssVarExpression = $"var({cssPropertyName}, {fallbackOpacity})";
+
+		return $"@({opacityOverridePropertyName}?.ToString(CultureInfo.InvariantCulture) ?? \"{cssVarExpression}\")";
 	}
 }
